ecd210e5f64680ae1b98b516c198992a8324ee96
Pietro Cartaxo Villas Bôas <pietrocartaxovillasboas@gmail.com>
2026-07-27 18:43:26 -0300

    Desafios RC

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorNoEn.java
@@ -0,0 +1,70 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp()
+public class MotorNoEn extends LinearOpMode {
+    private DcMotor motorNoEn;
+
+    boolean invertido = false;
+    boolean bumperAnterior = false;
+
+    @Override
+    public void runOpMode() {
+        motorNoEn = hardwareMap.get(DcMotor.class, "MotorEn");
+
+        waitForStart();
+
+//        ===Desafio 1 ===
+//
+//        while (opModeIsActive()) {
+//            if (gamepad1.a) {
+//                motorNoEn.setPower(1.0);
+//            } else if(gamepad1.b){
+//                motorNoEn.setPower(0.0);
+//            }
+//            telemetry.addData("Motor Power", motorNoEn.getPower());
+//            telemetry.update();
+
+//        ===Desafio 2===
+//
+//            while (opModeIsActive()){
+//                motorNoEn.setPower(gamepad1.right_trigger);
+//                telemetry.addData("Motor Power", motorNoEn.getPower());
+//                telemetry.update();
+
+//        ===Desafio 3===
+//
+//                while(opModeIsActive()){
+//                    if(gamepad1.dpad_up){
+//                        motorNoEn.setPower(1.0);
+//                    } else if(gamepad1.dpad_down){
+//                        motorNoEn.setPower(-1.0);
+//                    } else {
+//                        motorNoEn.setPower(0.0);
+//                    }
+//                    telemetry.addData("Motor Power", motorNoEn.getPower());
+//                    telemetry.update();
+//                }
+
+//        ===Desafio 4===
+//        while(opModeIsActive()){
+//            if(gamepad1.right_bumper && !bumperAnterior){
+//                invertido = !invertido;
+//            }
+//            bumperAnterior = gamepad1.right_bumper;
+//
+//            if (invertido) {
+//                motorNoEn.setPower(-1.0);
+//            } else {
+//                motorNoEn.setPower(1.0);
+//            }
+//
+//            telemetry.addData("Motor Power", motorNoEn.getPower());
+//            telemetry.addData("Invertido", invertido);
+//            telemetry.update();
+//        }
+    }
+}
